15d28f9d24d749457d178a9cb063d2d9d8ccae5d
Ghazale Noroozi <ghazalnoroozi27@yahoo.com>
2019-05-09 23:53:14 +0430

    Main

---
+++ Main.cs
@@ -0,0 +1,9 @@
+Class Main(){
+    static void Main(strings[] args){
+        Console.WriteLine("calculator:\n");
+        boolean isContinued = true;
+        while(isContinued){
+            isContinued = Input.getInput();
+        }           
+    }
+}
